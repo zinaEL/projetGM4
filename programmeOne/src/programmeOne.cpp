630a5323169b05195b21f8b49fb9deab8817c29b
zinaEL <zina.guedria@gmail.com>
2017-03-17 10:32:06 +0100

    ajouter une ligne d'affichage

---
+++ programmeOne/src/programmeOne.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : programmeOne.cpp
+// Author      : Zina EL Guedria
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "premiere modification";
+	return 0;
+}
